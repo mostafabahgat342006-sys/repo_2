88bdddb53a49a618c783020f18ac1bfff3bee94a
mostafa bahgat <mostafabahgat342006@gmail.com>
2026-09-08 09:14:54 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
-﻿namespace c__ass2;
+﻿
+using System.Text;
+namespace c__ass2;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string title = "clean code";
+        string upperTitle = title.ToUpper();
+
+        Console.WriteLine(title);
+        Console.WriteLine(upperTitle);
+
+
+
+
+
+
+
+
     }
 }
